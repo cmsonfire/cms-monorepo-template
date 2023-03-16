718f81551c5c781798c4f2cb2bbe4e3ff0240e49
talves <tony@3alves.com>
2023-03-16 10:06:04 -0700

    update: add some comments for ts

--- packages/cms-library-template/index.ts
+++ packages/cms-library-template/index.ts
@@ -3,9 +3,8 @@ let hello: string = "World";
 /**
  * A function that sets the name of the message.
  *
- * @type {(name: Required<string>) => void}
  */
-const setName = (name:Required<string>) => {
+const setName = (name:Required<string>):void => {
     if (!name) throw "Invalid name value";
     hello = name;
 }
@@ -13,7 +12,6 @@ const setName = (name:Required<string>) => {
 /**
  * A function that gets the message.
  *
- * @type {(name?: string) => string}
  */
 const getMessage = (name?: string ):string =>  {
     if (name) setName(name);
